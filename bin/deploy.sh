71e0fc5b9b346d2768daacf6e10e954c6b2a9b68
algm <piticonejo@gmail.com>
2017-04-23 14:26:41 +0200

    travis setup

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+echo "Testing"
+npm test
+
 set -e
 BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 if [[ $BRANCH == 'master' ]]; then
@@ -10,6 +14,7 @@ if [ -z ${STAGE+x} ]; then
   echo "Not deploying changes";
   exit 0;
 fi
+
 echo "Deploying from branch $BRANCH to stage $STAGE"
 npm prune --production  #remove devDependencies
 sls deploy --stage $STAGE --region $AWS_REGION
